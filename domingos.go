850560d96ed5a0c905743844b527b0f11b5edd05
Patrick Dube <pdube@cloudops.com>
2016-09-01 17:54:06 -0400

    Go fmt

--- domingos.go
+++ domingos.go
@@ -1,25 +1,30 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "github.com/gorilla/mux"
-    "github.com/boltdb/bolt"
+	"github.com/boltdb/bolt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+const (
+	DOMINOS_URL = "https://order.dominos.ca/power/"
 )
 
 func YourHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Gorilla!\n"))
+	w.Write([]byte("Gorilla!\n"))
+
 }
 
 func main() {
-    r := mux.NewRouter()
-    // Routes consist of a path and a handler function.
-    r.HandleFunc("/", YourHandler)
-    db, err := bolt.Open("my.db", 0600, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
-    // Bind to a port and pass our router in
-    log.Fatal(http.ListenAndServe(":8000", r))
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
