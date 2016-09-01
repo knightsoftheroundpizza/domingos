819e94a6635c699edd3cb0fad1b97f1940017346
Franz Garcia <fgarcia@cloudops.com>
2016-09-01 18:45:36 -0400

    Added orders db

--- domingos.go
+++ domingos.go
@@ -11,23 +11,22 @@ const (
 	DOMINOS_URL = "https://order.dominos.ca/power/"
 )
 
-
 func main() {
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    db, err := bolt.Open("my.db", 0600, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    oh := OrdersHandler{db: db}
-    // Routes consist of a path and a handler function.
-    r.HandleFunc("/orders", oh.GetOrdersHandler).Methods("GET")
-    r.HandleFunc("/orders", oh.PostOrdersHandler).Methods("POST")
-    r.HandleFunc("/orders/{id}", oh.PostOrdersOnIdHandler).Methods("POST")
+	oh := CreateOrdersHandler(db)
 
-    // Bind to a port and pass our router in
-    log.Fatal(http.ListenAndServe(":8000", r))
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/orders", oh.GetOrdersHandler).Methods("GET")
+	r.HandleFunc("/orders", oh.PostOrdersHandler).Methods("POST")
+	r.HandleFunc("/orders/{id}", oh.PostOrdersOnIdHandler).Methods("POST")
 
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
--- orders.go
+++ orders.go
@@ -1,16 +1,93 @@
 package main
 
 import (
-    "net/http"
-    "github.com/boltdb/bolt"
+	"encoding/json"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"net/http"
 )
 
-type OrdersHandler struct {
+const ORDER_BUCKET = "orders"
+
+type Order struct {
+	Id       string `json:id`
+	Status   string `json:status`
+	Customer string `json:customer`
+}
+
+type OrdersDb struct {
 	db *bolt.DB
 }
 
-func (oh *OrdersHandler) Init() {
+func (o *OrdersDb) Init() {
+	o.db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte(ORDER_BUCKET))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
+}
 
+func (o *OrdersDb) Find(id string) Order {
+	result := Order{}
+	o.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(ORDER_BUCKET))
+		v := b.Get([]byte(id))
+		json.Unmarshal(v, &result)
+		return nil
+	})
+	return result
+}
+
+func (o *OrdersDb) FindAll() []Order {
+	result := []Order{}
+	o.db.View(func(tx *bolt.Tx) error {
+		// Assume bucket exists and has keys
+		b := tx.Bucket([]byte(ORDER_BUCKET))
+		b.ForEach(func(k, v []byte) error {
+			e := Order{}
+			json.Unmarshal(v, &e)
+			result = append(result, e)
+			return nil
+		})
+		return nil
+	})
+	return result
+}
+
+func (o *OrdersDb) Remove(id string) bool {
+	success := true
+	o.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(ORDER_BUCKET))
+		err := b.Delete([]byte(id))
+		success = (err != nil)
+		return err
+	})
+	return success
+}
+
+func (o *OrdersDb) Insert(order Order) bool {
+	success := true
+	o.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(ORDER_BUCKET))
+		fmt.Println(b)
+		m, _ := json.Marshal(order)
+		err := b.Put([]byte(order.Id), m)
+		success = (err != nil)
+		return err
+	})
+	return success
+}
+
+type OrdersHandler struct {
+	ordersDb *OrdersDb
+}
+
+func CreateOrdersHandler(db *bolt.DB) *OrdersHandler {
+	odb := &OrdersDb{db: db}
+	odb.Init()
+	return &OrdersHandler{ordersDb: odb}
 }
 
 func (oh *OrdersHandler) GetOrdersHandler(w http.ResponseWriter, r *http.Request) {
